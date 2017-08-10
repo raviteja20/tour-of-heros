9de73b971eaa7a27071024545ced3cde2e48d4a7
teja@predictronics <teja@predictronics.com>
2017-08-10 00:41:28 -0400

    Completed getting data from remote service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
-import { Component } from '@angular/core';
-import {Hero} from './hero';
-import {HeroDetailComponent} from './hero-detail.component' 
-
-const HEROES: Hero[] = [
-	{ id: 11, name: 'Mr. Nice' },
-	{ id: 12, name: 'Narco' },
-	{ id: 13, name: 'Bombasto' },
-	{ id: 14, name: 'Celeritas' },
-	{ id: 15, name: 'Magneta' },
-	{ id: 16, name: 'RubberMan' },
-	{ id: 17, name: 'Dynama' },
-	{ id: 18, name: 'Dr IQ' },
-	{ id: 19, name: 'Magma' },
-	{ id: 20, name: 'Tornado' }
-];
+import { Component,OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -77,15 +65,27 @@ const HEROES: Hero[] = [
 		    border-radius: 4px 0 0 4px;
 		  }
 		`],
+  providers: [HeroService]
 })
 
-
-export class AppComponent  { 
+export class AppComponent implements OnInit { 
 	title = 'Tour of Heroes';
-	selectedHero: Hero;
-	heroes = HEROES;
+	selectedHero: Hero;	
+	heroes: Hero[];
+	//heroes = HEROES;
+
+	constructor(private heroService:HeroService) {}
+
+	getHeroes(): void {
+		this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
 
 	onSelect(hero:Hero): void{
 		this.selectedHero = hero;
 	}
+
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 }
+
