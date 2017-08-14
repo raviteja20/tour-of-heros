9e86e831c1c133891e862f9ba7cce2edc7ee258b
teja@predictronics <teja@predictronics.com>
2017-08-14 00:03:43 -0400

    checking for master change

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,7 +15,7 @@ export class HeroService {
 	             .then(response => response.json().data as Hero[])
 	             .catch(this.handleError);
 	}
-
+	//error handling function
 	private handleError(error: any): Promise<any> {
 	  console.error('An error occurred', error); // for demo purposes only
 	  return Promise.reject(error.message || error);
